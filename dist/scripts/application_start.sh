44d195ce8dc8a7d43a47ecc4039a2f8628cf20da
cicdaws <55483376+cicdaws@users.noreply.github.com>
2019-10-14 16:43:01 +0530

    Update application_start.sh

--- dist/scripts/application_start.sh
+++ dist/scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#cd /home/ubuntu/nodejs/CiCdPipeline/
+cd /home/ubuntu/nodejs/CiCdPipeline/
 #node /home/ubuntu/nodejs/CiCdPipeline/app.js
-forever start /home/ubuntu/nodejs/CiCdPipeline/app.js
+forever start app.js
