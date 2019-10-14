5ae9df261943ce76810a711e23ff7ecb1417e88e
cicdaws <55483376+cicdaws@users.noreply.github.com>
2019-10-14 15:55:26 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 //const api = require('./server/routes/api');
 const app = express();
 
-#app.use(express.static(path.join(__dirname,'/home/ubuntu/nodejs/CiCdPipeline')));
+//app.use(express.static(path.join(__dirname,'/home/ubuntu/nodejs/CiCdPipeline')));
 
 app.get('/*',(req, res) => {
 res.sendFile(path.join(__dirname,'index.html'));
